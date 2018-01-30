66d890ab9bd7027e034371899522f48efec6951f
xSlothx <35970992+xSlothx@users.noreply.github.com>
2018-01-30 23:50:42 +0000

    Update HelloWorld.java
    Changed public to private

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class HelloWorld {
-   public static void main(String[] args) {
+private class HelloWorld {
+   private static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
 
