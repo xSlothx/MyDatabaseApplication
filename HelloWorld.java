cff7af90d108a9c8108a37bb49375fc89cd72bbf
xSlothx <35970992+xSlothx@users.noreply.github.com>
2018-01-30 23:54:45 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 private class HelloWorld {
-   private static void main(String[] args) {
+   private static void main(final String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
 
