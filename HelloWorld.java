9e4c641bccd2bf20d52e91ec642669182a5d9394
xSlothx <35970992+xSlothx@users.noreply.github.com>
2018-01-30 14:22:02 -0800

    Add files via upload
    My first Java program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+
+      Scanner scanner = new Scanner(System.in);
+      String sentence = scanner.nextLine();
+String sentence2 = scanner.nextLine();
+      
+   }
+}
