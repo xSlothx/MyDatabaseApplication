aae0b0ed3da006ccf8f01da4acdde94749f52086
xSlothx <35970992+xSlothx@users.noreply.github.com>
2018-02-13 15:36:25 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,8 @@
 import java.util.Scanner;
-/**
-@author Mathew Phan. This is the Toad Developer modification
-*/
+
 public class HelloWorld {
-   /**
-   This is the main program.
-   @param args is empty.
-   */
-   public static void main(final String[] args) {
+
+   public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
 
