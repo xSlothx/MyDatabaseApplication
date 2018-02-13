73823c32bec7d3cfbed64465262a3a5fd9355c1d
xSlothx <35970992+xSlothx@users.noreply.github.com>
2018-02-13 15:31:40 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HelloWorld {
    /**
    This is the main program.
-   @param args is empty.
+   //@param args is empty.
    */
    public static void main(final String[] args) {
       // Prints "Hello, World" in the terminal window.
