d4bba3e39632118e00c85e4a3f79a73ff65f8592
xSlothx <mathew.phan@quest.com>
2018-02-14 09:59:58 -0800

    Created HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
-import java.util.Scanner;
-
 public class HelloWorld {
-/*This is an update made from my local working directory*/
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-      
+      System.out.println("Hello, World!");
    }
 }
